78bf03fe439bd286146003b6d8694f039ac87bb6
James Newman <jamesmnewman1@gmail.com>
2014-04-17 23:26:05 -0700

    setting up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chadder::Application.config.secret_key_base = '708177b21cccac90ccbb704f1c9ce8183c41cd307c78807ee458629a2d57bd6a55daa694bc978e03c3d8dce1b7e9a5fb35ab55c3c399181a18288c8b215a5126'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chadder::Application.config.session_store :cookie_store, key: '_chadder_session'
