602075c4428749a515d9d704930f0c4c83131bda
James Newman <jamesmnewman1@gmail.com>
2014-08-02 18:16:40 -0700

    set up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,5 @@ class HomeController < ApplicationController
 
 	def index
 	end
-
-	def chat
-	end
-
-	def sign_up
-	end
-
-	def sign_out
-	end
+	
 end
---
+++ vendor/bundle/gems/pg-0.17.1/ext/siteconf20140802-4386-17v0fgt.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140802-4386-1v7gg4l"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
